c3d0d048dfa8cb355b9e61540e14c46fb66539db
Geoff Butler <geoffbutler@users.noreply.github.com>
2016-07-26 00:13:21 +1200

    Add code

---
+++ FsFun.Tests/Tests.fs
@@ -0,0 +1,25 @@
+﻿namespace FsFun.Tests
+
+module ProgramShould = 
+    open Program
+    open Xunit
+
+
+    [<Fact>]
+    let return_false() = 
+        
+        let expected = false
+
+        let result = Program.return_false
+
+        Assert.StrictEqual(expected, result)
+
+
+    [<Fact>]
+    let return_true() = 
+        
+        let expected = true
+
+        let result = Program.return_true
+
+        Assert.StrictEqual(expected, result)
---
+++ FsFun/Program.fs
@@ -0,0 +1,11 @@
+﻿module Program = 
+
+    let return_false = 
+        false
+
+    let return_true = 
+        true
+
+    [<EntryPoint>]
+    let main argv =         
+        0
